80f456ee1ecf15d83548cd0f32ac10db23c3f6b8
MENGYU MI <daqianduan@sina.com>
2016-07-15 23:54:12 +0800

    add project config

---
+++ src/js/components/calendar.jsx
@@ -0,0 +1,5 @@
+import Calendar from 'rc-calendar';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const container = document.getElementById('container');
+ReactDOM.render(<Calendar />, container);
