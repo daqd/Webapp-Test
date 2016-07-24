52ed26911a54284fe6d3a7844ba264a696654576
daqd <daqianduan@sina.com>
2016-07-24 19:43:38 +0800

    更新

--- src/js/components/calendar.jsx
+++
@@ -1,5 +0,0 @@
-import Calendar from 'rc-calendar';
-import React from 'react';
-import ReactDOM from 'react-dom';
-const container = document.getElementById('container');
-ReactDOM.render(<Calendar />, container);
